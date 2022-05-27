2a2802068b76104daf422481346d51c96804a01d
famzila <famzil.contact@gmail.com>
2022-05-27 17:39:33 +0200

    First template overview

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from './material.module';
 import { TruncatePipe } from './truncate.pipe';
+import { TableOfContentComponent } from './table-of-content/table-of-content.component';
 
 
 
 @NgModule({
-  declarations: [TruncatePipe],
-  exports: [TruncatePipe, CommonModule, MaterialModule],
+  declarations: [TruncatePipe, TableOfContentComponent],
+  exports: [TruncatePipe, CommonModule, MaterialModule, TableOfContentComponent],
   imports: [
     CommonModule, 
     MaterialModule
---
+++ src/app/shared/table-of-content/table-of-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'table-of-content',
+  templateUrl: './table-of-content.component.html',
+  styleUrls: ['./table-of-content.component.scss']
+})
+export class TableOfContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
